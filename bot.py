14e3f8e7f83ed658e09df5b9d457000d0a0b6743
Purr404 <callmewhoosh@gmail.com>
2026-02-04 17:22:46 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -207,7 +207,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 translator = SelectiveTranslator()
 
 # ========== SOLUTION 1: THREAD-BASED (Recommended) ==========
